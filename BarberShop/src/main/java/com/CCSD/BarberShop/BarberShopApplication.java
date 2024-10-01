0d468c76c63ef4e9f1801487eebc10052865cdc0
pasraisyr <154299276+pasraisyr@users.noreply.github.com>
2024-10-01 16:02:33 +0800

    Project

--- BarberShop/src/main/java/com/CCSD/BarberShop/BarberShopApplication.java
+++ BarberShop/src/main/java/com/CCSD/BarberShop/BarberShopApplication.java
@@ -1,4 +1,4 @@
-package com.CCSD.BarberShop;
+package com.ccsd.BarberShop;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- BarberShop/src/test/java/com/CCSD/BarberShop/BarberShopApplicationTests.java
+++ BarberShop/src/test/java/com/CCSD/BarberShop/BarberShopApplicationTests.java
@@ -1,4 +1,4 @@
-package com.CCSD.BarberShop;
+package com.ccsd.BarberShop;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
